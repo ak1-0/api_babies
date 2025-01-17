5753b4d74ee52c2e0f0a26618049693b32224496
Alina Kornilova <ak1.0@yandex.ru>
2025-01-17 20:51:13 +0300

    Unnecessary lines have been deleted

--- src/test/java/org/example/SimpleUITest.java
+++ src/test/java/org/example/SimpleUITest.java
@@ -4,13 +4,16 @@ import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import java.nio.channels.Selector;
 
 import static com.codeborne.selenide.Selenide.element;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class SimpleUITest {
+
     @Test
     public void userCanNotCreateAccountWithNameAndSurnameOnly(){
         //Подготовка
@@ -27,21 +30,11 @@ public class SimpleUITest {
         //Шаги теста: Кнопка регистрации
         SelenideElement registerButton = element(Selectors.byValue("Register"));
         registerButton.click();
-        //Шаги теста: Имя
 
         //Проверка ожидаемого результата: Адрес
         SelenideElement addressError = element(Selectors.byId("customer.address.street.errors"));
         //addressError.shouldHave(Condition.exactText("Address is required."));
         addressError.shouldHave(Condition.cssClass("error"));
-
-        //Город: City is required. ("customer.address.city.errors") ("City is required.")
-        //Область: State is required. ("customer.address.state.errors") ("State is required.")
-        //Почтовый индекс: Zip Code is required. ("customer.address.zipCode.errors") ("Zip Code is required.")
-        //Телефон: ("customer.phoneNumber") ("")
-        //Номер страхования: Social Security Number is required. ("customer.ssn.errors") ("Social Security Number is required.")
-        //Юзернейм: Username is required. ("customer.username.errors") ("Username is required.)
-        //Пароль: Password is required. ("customer.password.errors") ("Password is required.")
-        //Подтверждение пароля: Password confirmation is required. ("repeatedPassword.errors") ("Password confirmation is required.")
 
         // Проверка ожидаемого результата: Город
         SelenideElement cityError = element(Selectors.byId("customer.address.city.errors"));
